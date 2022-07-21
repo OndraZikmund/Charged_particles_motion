264be30c1c11a262a568afe7a43df82e66c875dd
OndraZikmund <103008155+OndraZikmund@users.noreply.github.com>
2022-07-22 00:09:23 +0200

    Update test_E_constantxB_constant_drift.py

--- test_E_constantxB_constant_drift.py
+++ test_E_constantxB_constant_drift.py
@@ -6,6 +6,7 @@ Created on Sun May 15 00:39:11 2022
 """
 
 
+
 import os
 import math
 os.chdir('c:\\Users\\Uzivatel\Desktop\Plasma')
@@ -18,28 +19,27 @@ from field_force import Field_force
 
 # Test
 
-position= Vector([0,-2,0],3)
-#velocity = Vector([0,10,0.5],3)
-velocity = Vector([0,2,0],3)
+#position= Vector([0,-2,0],3)
+#velocity = Vector([0,2,0],3)
 
 
 dt = 0.001
-mass = 1
 
 x = []
 y = []
 z = []
 
 N = 20000
+
+values = Vector.interface()
+position = values[0]
+velocity = values[1]
+mass = values[2]
+
 for w in range(N):
     
-    field = Field_force(1,2,3,1,3,2)  # self, charge,B, dimension, E, directionB, directionE
-    
-    Lorentz_force = field.homogenous_magnetic_force(velocity)
-    acceleration = Lorentz_force.multiplication_scalar(1/mass)
-    acceleration = acceleration.addition(field.homogenous_electric_force(2).vector)
-    acceleration = Vector(acceleration,3)
-    
+    field = Field_force(3,1,[0,0,2],[0,1,0],1,1)  # self, charge,B, dimension, E, directionB, directionE, , G, directionG
+    acceleration = field.E_B_drift(mass, velocity)
     
     position = Vector(Integrator.Euler(position,velocity,acceleration,dt)[0],3)
     velocity = Vector(Integrator.Euler(position,velocity,acceleration,dt)[1],3)
@@ -48,9 +48,7 @@ for w in range(N):
     y.append(Integrator.Euler(position,velocity,acceleration,dt)[0][1])
     z.append(Integrator.Euler(position,velocity,acceleration,dt)[0][2])
     
-    if (w%100==0):
-        print(Integrator.Euler(position,velocity,acceleration,dt)[0][0])
-    
+
 T = [t*dt for t in range(N)]
 
 plt.plot(y,z, color  = "green")  
@@ -68,3 +66,4 @@ plt.ylim([-5, 5])
 ax.plot3D(x, y, z, 'blue')
 
 print("poloha x",x[0])
+
