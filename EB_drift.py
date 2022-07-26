50abfcff0c4b14058e8c025337efad8ecfaec0d7
OndraZikmund <103008155+OndraZikmund@users.noreply.github.com>
2022-07-26 14:57:22 +0200

    Update and rename test_E_constantxB_constant_drift.py to EB_drift.py

--- EB_drift.py
+++ EB_drift.py
@@ -4,9 +4,6 @@ Created on Sun May 15 00:39:11 2022
 
 @author: Uzivatel
 """
-
-
-
 import os
 import math
 os.chdir('c:\\Users\\Uzivatel\Desktop\Plasma')
@@ -15,6 +12,9 @@ import matplotlib.pyplot as plt
 from vector import Vector
 from Integrator import Integrator
 from field_force import Field_force
+from interface import Interface
+from plot import Plot
+
 
 
 # Test
@@ -31,15 +31,26 @@ z = []
 
 N = 20000
 
-values = Vector.interface()
+a = Vector([1,1,1],3)
+
+values = Interface.create_particle()
 position = values[0]
 velocity = values[1]
 mass = values[2]
+charge = values[3]
+
+
+values_field = Interface.create_elmag_field()
+
+dimension = values_field[0]
+E = values_field[1]
+B = values_field[2]
 
 for w in range(N):
     
-    field = Field_force(3,1,[0,0,2],[0,1,0],1,1)  # self, charge,B, dimension, E, directionB, directionE, , G, directionG
-    acceleration = field.E_B_drift(mass, velocity)
+
+    field = Field_force(3,B, E)  # self, dimension, charge, B , E
+    acceleration = field.E_B_drift(mass, velocity, charge)
     
     position = Vector(Integrator.Euler(position,velocity,acceleration,dt)[0],3)
     velocity = Vector(Integrator.Euler(position,velocity,acceleration,dt)[1],3)
@@ -49,21 +60,5 @@ for w in range(N):
     z.append(Integrator.Euler(position,velocity,acceleration,dt)[0][2])
     
 
-T = [t*dt for t in range(N)]
-
-plt.plot(y,z, color  = "green")  
-
-fig = plt.figure()
-ax = plt.axes(projection='3d')
-plt.title("Drift ve zkříženém elektromagnetickém poli")
-
-plt.xlabel("axe x")
-plt.ylabel("axe y")
-
-plt.xlim([0, 10])
-plt.ylim([-5, 5])
-
-ax.plot3D(x, y, z, 'blue')
-
-print("poloha x",x[0])
+Plot.plot_drift(x,y,z,N,dt)
 
